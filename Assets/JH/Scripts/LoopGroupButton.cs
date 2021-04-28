f03e6518719c6e4aee1050596a1d02a575310810
JongHyeon <73339750+Oct0193@users.noreply.github.com>
2021-04-28 19:48:53 +0900

    프로젝트 폴더 정리

---
+++ Assets/JH/Scripts/LoopGroupButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopGroupButton : MonoBehaviour
+{
+    public RecordPlayer rp;
+
+    public bool isLoop;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Controller")
+            rp.OnClickPlay(isLoop);
+    }
+}
