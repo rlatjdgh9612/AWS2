499ea4439e515e7bd68b959868fe4e03a779906a
udune <udune@naver.com>
2021-05-24 13:43:18 +0900

    TMI 발표용 버전 수정본_v02

--- Assets/Scripts/Ramp/RampRay.cs
+++ Assets/Scripts/Ramp/RampRay.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent (typeof (AudioSource))]
 public class RampRay : MonoBehaviour
 {
     private bool isOne = false;
@@ -31,42 +32,43 @@ public class RampRay : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, float.MaxValue))
         {
-            if (hit.transform.gameObject.CompareTag("Pad"))
+            if (hit.transform.gameObject.GetComponent<AudioSource>() != null)
             {
-                if (hit.transform.gameObject.GetComponent<AudioSource>() == null)
+                if (hit.transform.gameObject.GetComponent<AudioSource>().clip != null)
                 {
-                    return;
+                    if (hit.transform.gameObject.CompareTag("Pad"))
+                    {
+                        char divide = '_';
+                        string[] split = hit.transform.gameObject.GetComponent<AudioSource>().clip.name.Split(divide);
+                        string name1 = split[split.Length - 2];
+                        string name2 = Parsing(split.Last(), name1);
+                        string fullName = name1 + "_" + name2;
+
+                        if (!isOne)
+                        {
+                            soundInfo = Load("SoundInfo/SoundInfo");
+                            isOne = true;
+                        }
+
+                        dist = Vector3.Distance(transform.position, hit.point);
+                        multDist = Mathf.Pow(dist * 2.0f, 5.0f);
+
+                        soundInfo.GetComponentInChildren<Text>().text = fullName;
+                        soundInfo.GetComponentInChildren<Text>().color = new Color(1.0f, 1.0f, 1.0f, ((255.0f/multDist)/255.0f)*2);
+                        soundInfo.GetComponentInChildren<Image>().color = new Color(0.0f,225.0f/255.0f,1.0f,((255.0f/multDist)/255.0f)*2);
+                        soundInfo.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y, hit.transform.position.z) + (Vector3.up * 0.1f);
+                        soundInfo.transform.rotation = Quaternion.Euler(soundInfo.transform.eulerAngles.x, playerCam.transform.eulerAngles.y, soundInfo.transform.eulerAngles.z);
+                    }
+                    else
+                    {
+                        if (isOne)
+                        {
+                            soundInfo.GetComponentInChildren<Image>().color = new Color(0.0f,225.0f/255.0f,1.0f,0.0f);
+                            Destroy(soundInfo);
+                        }
+                        isOne = false;
+                    }
                 }
-
-                char divide = '_';
-                string[] split = hit.transform.gameObject.GetComponent<AudioSource>().clip.name.Split(divide);
-                string name1 = split[split.Length - 2];
-                string name2 = Parsing(split.Last(), name1);
-                string fullName = name1 + "_" + name2;
-
-                if (!isOne)
-                {
-                    soundInfo = Load("SoundInfo/SoundInfo");
-                    isOne = true;
-                }
-
-                dist = Vector3.Distance(transform.position, hit.point);
-                multDist = Mathf.Pow(dist * 2.0f, 5.0f);
-
-                soundInfo.GetComponentInChildren<Text>().text = fullName;
-                soundInfo.GetComponentInChildren<Text>().color = new Color(1.0f, 1.0f, 1.0f, ((255.0f/multDist)/255.0f)*2);
-                soundInfo.GetComponentInChildren<Image>().color = new Color(0.0f,225.0f/255.0f,1.0f,((255.0f/multDist)/255.0f)*2);
-                soundInfo.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y + 0.1f, hit.transform.position.z);
-                soundInfo.transform.rotation = Quaternion.Euler(soundInfo.transform.eulerAngles.x, playerCam.transform.eulerAngles.y, soundInfo.transform.eulerAngles.z);
-            }
-            else
-            {
-                if (isOne)
-                {
-                    soundInfo.GetComponentInChildren<Image>().color = new Color(0.0f,225.0f/255.0f,1.0f,0.0f);
-                    Destroy(soundInfo);
-                }
-                isOne = false;
             }
         }
     }
